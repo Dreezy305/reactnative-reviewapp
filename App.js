e0a773ae6e27d924ee4f8ad7646bc29ed4dc7def
Idris Bankole <bankoleidris@gmail.com>
2021-11-24 23:41:28 +0100

    loaded custom fonts

--- App.js
+++ App.js
@@ -1,23 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import * as Font from "expo-font";
-import { AppLoading } from "expo";
+// import * as Font from "expo-font";
+// import { AppLoading } from "expo";
 import Home from "./screens/home";
 
-const getFonts = () =>
-  Font.loadAsync({
-    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
-    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
-  });
+// const getFonts = () =>
+//   Font.loadAsync({
+//     "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+//     "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+//   });
 
 export default function App() {
-  const [fontLoaded, setFontLoaded] = useState(false);
-
-  if (fontLoaded) {
-    return <Home />;
-  } else {
-    <AppLoading startAsync={ }/>;
-  }
+  // const [fontLoaded, setFontLoaded] = useState(false);
 
   return <Home />;
 }
