2ed640764efe2e5fdca6c097a7346b1f447d6440
Idris Bankole <bankoleidris@gmail.com>
2021-11-25 17:48:25 +0100

    react-navigation

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
+// import Navigator from "./routes/homeStack";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Home from "./screens/home";
 
--- routes/homeStack.js
+++ routes/homeStack.js
@@ -1,27 +1,17 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from "../screens/home";
-import ReviewDetail from "../screens/reviewDetail";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import Home from "../screens/home";
+// import ReviewDetail from "../screens/reviewDetail";
 
-const screens = {
-  Home: {
-    screen: Home,
-  },
-  ReviewDetail: {
-    screen: ReviewDetail,
-  },
-};
+// const screens = {
+//   Home: {
+//     screen: Home,
+//   },
+// //   ReviewDetail: {
+//     screen: ReviewDetail,
+//   },
+// };
 
-const HomeStack = createNativeStackNavigator(screens);
+// const HomeStack = createNativeStackNavigator(screens);
 
-// export default function HomeStack() {
-//   return (
-//     <View>
-//       <Text></Text>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({});
+// export default NavigationContainer(HomeStack);
