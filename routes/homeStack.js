f8c0bdf0ab6ff8a051544a55933d7a4f240cf6b8
Idris Bankole <bankoleidris@gmail.com>
2021-11-26 00:01:01 +0100

    navigation options

--- routes/homeStack.js
+++ routes/homeStack.js
@@ -6,12 +6,31 @@ import ReviewDetail from "../screens/reviewDetail";
 const screens = {
   Home: {
     screen: Home,
+    navigationOptions: {
+      title: "Home screen",
+      // headerStyle: {
+      //   backgroundColor: "#eee",
+      // },
+    },
   },
   ReviewDetail: {
     screen: ReviewDetail,
+    navigationOptions: {
+      title: "Review Details",
+      // headerStyle: {
+      //   backgroundColor: "#f4511e",
+      // },
+    },
   },
 };
 
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+  defaultNavigationOptions: {
+    headerTintColor: "#444",
+    headerStyle: {
+      backgroundColor: "#eee",
+    },
+  },
+});
 
 export default createAppContainer(HomeStack);
