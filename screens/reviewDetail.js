57a3a6a72f9a8afccde155d75217065db753ab14
Idris Bankole <bankoleidris@gmail.com>
2021-11-25 23:39:10 +0100

    passing data between screens

--- screens/reviewDetail.js
+++ screens/reviewDetail.js
@@ -2,10 +2,6 @@ import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function ReviewDetail({ navigation }) {
-  console.log(navigation, "vv");
-  // const pressHandler = () => {
-  //   navigation.goBack();
-  // };
   return (
     <View style={styles.container}>
       <Text>{navigation.getParam("title")}</Text>
