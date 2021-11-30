ef1f752dbda0a02612389a550e4b3ea551e5aec4
Idris Bankole <bankoleidris@gmail.com>
2021-11-30 23:36:04 +0100

    loaded images

--- screens/reviewDetail.js
+++ screens/reviewDetail.js
@@ -9,9 +9,13 @@ export default function ReviewDetail({ navigation }) {
         <Text>{navigation.getParam("title")}</Text>
         <Text>{navigation.getParam("body")}</Text>
         {/* <Text>{navigation.getParam("rating")}</Text> */}
-        <View>
+        <View style={styles.rating}>
           <Text>Rating: </Text>
-          <Image source={require("../assets/rating-1.png")} />
+          <Image
+            source={require(`../assets/rating-${navigation.getParam(
+              "rating"
+            )}.png`)}
+          />
         </View>
       </Card>
     </View>
@@ -22,4 +26,5 @@ const styles = StyleSheet.create({
   container: {
     padding: 24,
   },
+  rating: {},
 });
--- styles/global.js
+++ styles/global.js
@@ -16,3 +16,9 @@ export const globalStyles = StyleSheet.create({
     lineHeight: 20,
   },
 });
+
+export const images = {
+  ratings: {
+    1: require("../assets/rating-1.png"),
+  },
+};
