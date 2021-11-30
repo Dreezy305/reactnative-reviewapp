b98b4fa46c37ef8d9b18ae2fd1c05a6727f784a9
Idris Bankole <bankoleidris@gmail.com>
2021-12-01 00:00:16 +0100

    loaded images

--- screens/reviewDetail.js
+++ screens/reviewDetail.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 import Card from "../shared/card";
-import { images } from "../styles/global";
+import { images, globalStyles } from "../styles/global";
 
 export default function ReviewDetail({ navigation }) {
   const rating = navigation.getParam("rating");
 
   return (
-    <View style={styles.container}>
+    <View style={globalStyles.container}>
       <Card>
         <Text>{navigation.getParam("title")}</Text>
         <Text>{navigation.getParam("body")}</Text>
         {/* <Text>{navigation.getParam("rating")}</Text> */}
-        <View>
+        <View style={styles.rating}>
           <Text>Rating: </Text>
           <Image source={images.ratings[rating]} />
         </View>
@@ -24,5 +24,13 @@ export default function ReviewDetail({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     padding: 24,
+  },
+  rating: {
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: "#eee",
   },
 });
