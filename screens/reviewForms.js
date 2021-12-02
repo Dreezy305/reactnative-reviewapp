5753f781d1893df07f9f254eb73989c4760bf12d
Idris Bankole <bankoleidris@gmail.com>
2021-12-02 15:51:42 +0100

    forms

---
+++ screens/reviewForms.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { globalStyles } from "../styles/global";
+import { StyleSheet, Text, View } from "react-native";
+import { Formik } from "formik";
+
+export default function reviewForms() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
